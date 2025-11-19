73925c999ad4839c3a17316e85e0a7457534ffeb
wallacespimenta <wallace.s.pimenta@gmail.com>
2025-11-19 00:39:16 -0300

    FEAT: Implementacao tratamento de resposta padronizada nos retornos da api.

--- src/main/java/com/sistemabancario/transferenciapix/dto/OutputSuccessDTO.java
+++ src/main/java/com/sistemabancario/transferenciapix/dto/OutputSuccessDTO.java
@@ -1,40 +1,20 @@
 package com.sistemabancario.transferenciapix.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OutputSuccessDTO {
 
     private boolean success;
     private TransferenciaPixResponseDTO data;
     private String timestamp;
 
-    public boolean isSuccess() {
-        return success;
-    }
-
-    public TransferenciaPixResponseDTO getData() {
-        return data;
-    }
-
-    public String getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public void setData(TransferenciaPixResponseDTO data) {
-        this.data = data;
-    }
-
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
-
-    public OutputSuccessDTO(boolean success, TransferenciaPixResponseDTO data, String timestamp) {
-
-        this.success = success;
-        this.data = data;
-        this.timestamp = timestamp;
-    }
 }
 
