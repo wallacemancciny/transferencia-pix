f6b229a472448a395d3f83a3256b15af22b15d5f
wallacespimenta <wallace.s.pimenta@gmail.com>
2025-11-19 22:22:34 -0300

    FEAT: Remoção de imports não utilizadas

--- src/main/java/com/sistemabancario/transferenciapix/controller/TransferenciaPixController.java
+++ src/main/java/com/sistemabancario/transferenciapix/controller/TransferenciaPixController.java
@@ -3,23 +3,13 @@ package com.sistemabancario.transferenciapix.controller;
 import com.sistemabancario.transferenciapix.dto.OutputSuccessDTO;
 import com.sistemabancario.transferenciapix.dto.TransferenciaPixRequestDTO;
 import com.sistemabancario.transferenciapix.dto.TransferenciaPixResponseDTO;
-import com.sistemabancario.transferenciapix.entity.TransferenciaPix;
 import com.sistemabancario.transferenciapix.service.TransferenciaPixService;
 import jakarta.validation.Valid;
-import org.apache.coyote.Response;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.Instant;
 import java.util.List;
 
-/**
- * Classe responsável por expor os endpoints da API de Transferência Pix.
- * Ela recebe as requisições HTTP (via JSON), chama o Service e retorna a resposta.
- */
 @RestController // Indica que esta classe é um controller REST (retorna JSON)
 @RequestMapping("/api/transferencias") // Define o "prefixo" para todos os endpoints dessa API
 public class TransferenciaPixController {
@@ -31,22 +21,6 @@ public class TransferenciaPixController {
         this.service = service;
     }
 
-    /**
-     * Endpoint responsável por enviar uma nova transferência Pix.
-     * Método POST → cria um novo registro.
-     *
-     * Exemplo de chamada:
-     * POST /api/transferencias
-     * {
-     *   "chaveOrigem": "wallace@banco.com",
-     *   "nomeOrigem": "Wallace Santana",
-     *   "bancoOrigem": "Banco XPTO",
-     *   "chaveDestino": "joao@banco.com",
-     *   "nomeDestino": "João Almeida",
-     *   "valor": 250.75,
-     *   "mensagem": "Pagamento de serviço"
-     * }
-     */
     @PostMapping // Indica que é um endpoint HTTP POST
     public ResponseEntity<OutputSuccessDTO> enviar(@Valid @RequestBody TransferenciaPixRequestDTO transferencia) {
         // O JSON da requisição é convertido em um objeto TransferenciaPix automaticamente pelo Spring
@@ -55,13 +29,7 @@ public class TransferenciaPixController {
 
         return ResponseEntity.ok(new OutputSuccessDTO(dto));
     }
-    /**
-     * Endpoint responsável por listar todas as transferências Pix cadastradas.
-     * Método GET → retorna todos os registros.
-     *
-     * Exemplo:
-     * GET /api/transferencias
-     */
+
     @GetMapping
     public ResponseEntity<OutputSuccessDTO<List<TransferenciaPixResponseDTO>>> listar() {
 
@@ -71,13 +39,6 @@ public class TransferenciaPixController {
 
     }
 
-    /**
-     * Endpoint responsável por buscar uma transferência específica pelo código Pix.
-     * Método GET → busca um registro único.
-     *
-     * Exemplo:
-     * GET /api/transferencias/PIX-A1B2C3D4
-     */
     @GetMapping("/{codigo}")
     public ResponseEntity<OutputSuccessDTO> buscarPorCodigo(@PathVariable String codigo) {
         // @PathVariable = captura o valor da URL e passa pro método
@@ -95,14 +56,17 @@ public class TransferenciaPixController {
     }
 
     @DeleteMapping("/{codigotransacao}")
-    public ResponseEntity<String> deletarPorCodigoTransacao(@PathVariable String codigotransacao) {
+    public ResponseEntity<OutputSuccessDTO> deletarPorCodigoTransacao(@PathVariable String codigotransacao) {
         try {
             service.deletarPorCodigoTransacao(codigotransacao);
-            return ResponseEntity.ok("Transferência deletada com sucesso!");
+
+            ResponseEntity<OutputSuccessDTO> ok = ResponseEntity.ok(new OutputSuccessDTO("Transferencia " + codigotransacao + " deletada com sucesso!"));
+            return ok;
+
         } catch (RuntimeException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Erro ao deletar transferência: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new OutputSuccessDTO("Erro ao deletar transferência " + codigotransacao + " | " + e.getMessage()));
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro inesperado ao deletar transferência");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new OutputSuccessDTO("Erro inesperado ao deletar transferência " + codigotransacao + " | " + e.getMessage()));
         }
 
     }
